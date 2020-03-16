1b0d52a3b6694d27d85497f915fa04da6b3cf59a
Marcos Freitas <marcosfreitas@c4network.com.br>
2020-03-15 21:39:16 -0300

    :sparkles: Definição de chamadas dos métodos da camada de serviços e tratamento das respostas;

--- www/app/Http/Controllers/ClientController.php
+++ www/app/Http/Controllers/ClientController.php
@@ -4,38 +4,42 @@ namespace App\Http\Controllers;
 
 use App\Client;
 use Illuminate\Http\Request;
+use App\Services\ClientService;
+use App\Http\Requests\ClientStoreRequest;
+use App\Http\Requests\ClientUpdateRequest;
+use App\Http\Requests\ClientDestroyRequest;
 
 class ClientController extends Controller
 {
+    private $model_service;
+
+    public function __construct(ClientService $client_service)
+    {
+        $this->model_service = $client_service;
+    }
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
-    }
+        $index = $this->model_service->getByFilters($request->all());
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        return parent::response($index);
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Http\Requests\ClientStoreRequest  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ClientStoreRequest $request)
     {
-        //
+        $store = $this->model_service->store($request);
+
+        return parent::response($store);
     }
 
     /**
@@ -49,37 +53,29 @@ class ClientController extends Controller
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
+   /**
+     * Update the specified resource in storage.
      *
-     * @param  \App\Models\Client  $client
+     * @param  \App\Http\Requests\ClientUpdateRequest  $request
      * @return \Illuminate\Http\Response
      */
-    public function edit(Client $client)
+    public function update(ClientUpdateRequest $request)
     {
-        //
+        $store = $this->model_service->update($request);
+
+        return parent::response($store);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Client  $client
+     * @param  \App\Http\Requests\ClientUpdateRequest  $request
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Client $client)
+    public function destroy(ClientDestroyRequest $request)
     {
-        //
-    }
+        $destroy = $this->model_service->destroy($request);
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Client  $client
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Client $client)
-    {
-        //
+        return parent::response($destroy);
     }
 }
--- www/app/Http/Controllers/Controller.php
+++ www/app/Http/Controllers/Controller.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * Parse response HTTP level code
+     */
+    public function response($data)
+    {
+        return response($data, !$data['error'] ? JsonResponse::HTTP_OK : JsonResponse::HTTP_UNPROCESSABLE_ENTITY);
+    }
 }
