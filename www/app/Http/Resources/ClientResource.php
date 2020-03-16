d4656609091c7975aa47dc1591f4944bc39ed3c4
Marcos Freitas <marcosfreitas@c4network.com.br>
2020-03-15 21:40:32 -0300

    :sparkles: Construída Classe Resource para padronizar as respostas do Client

---
+++ www/app/Http/Resources/ClientResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ClientResource extends JsonResource
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'uuid' => $this->uuid,
+            'name' => $this->name,
+            'birth' => $this->birth,
+            'email' => $this->email,
+            'phone' => $this->phone,
+            'address' => $this->address,
+            'complement' => $this->complement,
+            'district' => $this->district,
+            'zip_code' => $this->zip_code,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at
+        ];
+    }
+}
