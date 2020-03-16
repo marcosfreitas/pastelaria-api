0752972d56ff3a3d9a54a1cafc8ea75119e1f5db
Marcos Freitas <marcosfreitas@c4network.com.br>
2020-03-15 21:43:37 -0300

    declarado client observer no app service provider para poder funcionar

--- www/app/Providers/AppServiceProvider.php
+++ www/app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Client;
+use App\Observers\ClientObserver;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Client::observe(ClientObserver::class);
     }
 }
